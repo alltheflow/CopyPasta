e87bf2b9965a04a004bdf879a4deb94e862125c2
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-10-29 10:10:41 +0100

    Initial Commit

---
+++ PasteFlow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PasteFlow
+//
+//  Created by Agnes Vasarhelyi on 29/10/15.
+//  Copyright © 2015 Agnes Vasarhelyi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PasteFlow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PasteFlow
+//
+//  Created by Agnes Vasarhelyi on 29/10/15.
+//  Copyright © 2015 Agnes Vasarhelyi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
