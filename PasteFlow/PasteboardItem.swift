e6cf7d0acdc308e61fe9450ff0a2dc36c2053bf6
bvic23 <bvic23@gmail.com>
2015-11-24 20:20:27 +0100

    Review fix

--- PasteFlow/PasteboardItem.swift
+++ PasteFlow/PasteboardItem.swift
@@ -8,10 +8,27 @@
 
 import Cocoa
 
+extension NSImage {
+    
+    public override func isEqualTo(object: AnyObject?) -> Bool {
+        guard let otherImage = object as? NSImage else {
+            return false
+        }
+        
+        guard let lhsiTiff = self.TIFFRepresentation,
+            let rhsiTiff = otherImage.TIFFRepresentation  else {
+            return false
+        }
+        
+        return lhsiTiff.isEqualToData(rhsiTiff)
+    }
+    
+}
+
 func ==(lhs: PasteboardItem, rhs: PasteboardItem) -> Bool {
     switch (lhs, rhs) {
     case (.Text(let str1), .Text(let str2)): return str1 == str2
-    case (.Image(let img1), .Image(let img2)): return img1 == img2
+    case (.Image(let img1), .Image(let img2)): return img1.isEqualTo(img2)
     case (.URL(let str1), .URL(let str2)): return str1 == str2
     default: return false
     }
