6aa8ed98df3709a1336da7e3bc57c4e95607ff10
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-12-03 16:12:08 +0100

    Add dummy test case

---
+++ PasteFlowTests/PasteboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  PasteboardTests.swift
+//  PasteFlow
+//
+//  Created by Agnes Vasarhelyi on 03/12/15.
+//  Copyright © 2015 Agnes Vasarhelyi. All rights reserved.
+//
+
+import XCTest
+
+class PasteboardTests: XCTestCase {
+
+    func testSomething() {
+        XCTAssertNotNil("asd", "shouldn't be nil")
+    }
+
+}
